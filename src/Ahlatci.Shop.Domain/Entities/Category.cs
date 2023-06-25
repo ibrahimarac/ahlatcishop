6ea4477aadd89b6a6d23f91ba2cdd57d91c23eda
halilibrahim.arac <halil.arac@rise-consulting.net>
2023-06-25 15:05:13 +0300

    Category ve Product için NagigationProperty eklendi.

--- src/Ahlatci.Shop.Domain/Entities/Category.cs
+++ src/Ahlatci.Shop.Domain/Entities/Category.cs
@@ -5,5 +5,9 @@ namespace Ahlatci.Shop.Domain.Entities
     public class Category : AuditableEntity
     {
         public string Name { get; set; }
+
+        //Navigation Property
+        public ICollection<Product> Products { get; set; }
+
     }
 }
--- src/Ahlatci.Shop.Domain/Entities/Product.cs
+++ src/Ahlatci.Shop.Domain/Entities/Product.cs
@@ -2,12 +2,16 @@
 
 namespace Ahlatci.Shop.Domain.Entities
 {
+
     public class Product : AuditableEntity
-    {
+    {       
         public int CategoryId { get; set; }
         public string Name { get; set; }
         public string Detail { get; set; }
         public int UnitInStock { get; set; }
         public decimal UnitPrice { get; set; }
+
+        //Navigation Property
+        public Category Category { get; set; }
     }
 }
