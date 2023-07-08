babcdaab23e0fccc8b980556bcef921b3f89e9d8
halilibrahim.arac <halil.arac@rise-consulting.net>
2023-07-08 17:58:25 +0300

    DbContext sınıfı oluşturuldu.

---
+++ src/Ahlatci.Shop.Persistence/Context/AhlatciContext.cs
@@ -0,0 +1,66 @@
+﻿using Ahlatci.Shop.Domain.Common;
+using Ahlatci.Shop.Domain.Entities;
+using Ahlatci.Shop.Persistence.Mappings;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ahlatci.Shop.Persistence.Context
+{
+    public class AhlatciContext : DbContext
+    {
+        public AhlatciContext(DbContextOptions<AhlatciContext> options) : base(options){ }
+
+        #region DbSet
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            modelBuilder.ApplyConfiguration(new AccountMapping());
+            modelBuilder.ApplyConfiguration(new AddressMapping());
+            modelBuilder.ApplyConfiguration(new CategoryMapping());
+            modelBuilder.ApplyConfiguration(new CityMapping());
+            modelBuilder.ApplyConfiguration(new CommentMapping());
+            modelBuilder.ApplyConfiguration(new CustomerMapping());
+            modelBuilder.ApplyConfiguration(new OrderMapping());
+            modelBuilder.ApplyConfiguration(new OrderDetailMapping());
+            modelBuilder.ApplyConfiguration(new ProductMapping());
+            modelBuilder.ApplyConfiguration(new ProductImageMapping());
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<AuditableEntity>().ToList())
+            {
+                switch (entry.State)
+                {
+                    //update
+                    case EntityState.Modified:
+                        entry.Entity.ModifiedDate = DateTime.Now;
+                        entry.Entity.ModifiedBy = "admin";
+                        break;
+                    //insert
+                    case EntityState.Added:
+                        entry.Entity.CreateDate = DateTime.Now;
+                        entry.Entity.CreatedBy = "admin";
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+    }
+}
---
+++ src/Ahlatci.Shop.Persistence/Mappings/CityMapping.cs
@@ -0,0 +1,18 @@
+﻿using Ahlatci.Shop.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ahlatci.Shop.Persistence.Mappings
+{
+    public class CityMapping : BaseEntityMapping<City>
+    {
+        public override void ConfigureDerivedEntityMapping(EntityTypeBuilder<City> builder)
+        {
+            builder.Property(x => x.Name)
+                .HasColumnName("NAME")
+                .HasColumnType("nvarchar(20)")
+                .HasColumnOrder(2)
+                .IsRequired();
+        }
+    }
+}
