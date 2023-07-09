846916252606264e89bb904210cc8916f7a14363
halilibrahim.arac <halil.arac@rise-consulting.net>
2023-07-09 17:59:11 +0300

    IGenericRepository giriş yapıldı

---
+++ src/Ahlatci.Shop.Persistence/Repositories/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+
+namespace Ahlatci.Shop.Persistence.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        //CRUD C=>Create, R=>Read, U=>Update, D=>Delete
+
+        #region Read
+
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderby = null, params Expression<Func<T, object>>[] includes);
+
+        #endregion
+
+
+        #region Create, Update, Delete
+
+
+
+        #endregion
+    }
+}
